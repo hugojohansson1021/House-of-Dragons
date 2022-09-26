1dc55ad6556393d8d6599ad13689321f0b5bf350
Hugo <hugo>
2022-09-26 19:27:24 +0200

    sparar

--- Signup/signup.js
+++ Signup/signup.js
@@ -9,10 +9,8 @@ const out1 = document.getElementById('output1');
 
 function fun1(){
 	out1.innerHTML = txt1.value;
-}
-
+};
 btn1.addEventListener('click', fun1)
-
 
 
 
@@ -22,9 +20,9 @@ const out2 = document.getElementById('output2');
 
 function fun2(){
 	out2.innerHTML = txt2.value;
-}
-
+};
 btn2.addEventListener('click', fun2)
+
 
 
 const txt3 = document.getElementById('text_box3');
@@ -33,6 +31,16 @@ const out3 = document.getElementById('output3');
 
 function fun3(){
 	out3.innerHTML = txt3.value;
-}
-
+};
 btn3.addEventListener('click', fun3)
+
+
+
+const txt4 = document.getElementById('text_box4');
+const btn4 = document.getElementById('btn4');
+const out4 = document.getElementById('output4');
+
+function fun4(){
+	out4.innerHTML = txt4.value;
+};
+btn4.addEventListener('click', fun4)
